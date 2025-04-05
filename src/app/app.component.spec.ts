2df025c6e541bde4c5b83ae1d81d116678c13574
coyotetoledo <tpierce8080@gmail.com>
2025-04-05 11:29:39 -0400

    Update project name and build output

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tpierce-portfolio' title`, () => {
+  it(`should have the 'dev-portfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tpierce-portfolio');
+    expect(app.title).toEqual('dev-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tpierce-portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dev-portfolio');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { FooterComponent } from './components/footer/footer.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'tpierce-portfolio';
+  title = 'dev-portfolio';
 }
